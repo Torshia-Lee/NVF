77f917293baea147f9c72bb4fc722c08a9f9b58c
Torshia Lee <torshwlee@gmail.com>
2024-12-03 20:49:54 -0600

    Added the create action for comments model

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,5 @@
 class CommentsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :set_comment, only: %i[ show edit update destroy ]
 
   # GET /comments or /comments.json
@@ -21,16 +22,14 @@ class CommentsController < ApplicationController
 
   # POST /comments or /comments.json
   def create
-    @comment = Comment.new(comment_params)
-
-    respond_to do |format|
-      if @comment.save
-        format.html { redirect_to @comment, notice: "Comment was successfully created." }
-        format.json { render :show, status: :created, location: @comment }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @comment.errors, status: :unprocessable_entity }
-      end
+    @lake = Lake.find(params[:lake_id])
+    @comment = @lake.comments.create(comment_params)
+    @comment.user = current_user
+    
+    if @comment.save
+      redirect_to lake_path(@lake), notice: "Comment was successfully created."
+    else
+      redirect_to lake_path(@lake), notice: "Comment was not created."
     end
   end
 
@@ -65,6 +64,6 @@ class CommentsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def comment_params
-      params.require(:comment).permit(:user_id_id, :body, :lake_id_id)
+      params.require(:comment).permit(:body)
     end
 end
