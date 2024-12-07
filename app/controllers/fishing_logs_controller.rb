69e1aaf9ba8440299f1820fcb5d65a4afd967b62
Torshia Lee <torshwlee@gmail.com>
2024-12-06 20:56:23 -0600

    Adding the logic to the create action for fishing log

--- app/controllers/fishing_logs_controller.rb
+++ app/controllers/fishing_logs_controller.rb
@@ -14,6 +14,7 @@ class FishingLogsController < ApplicationController
   # GET /fishing_logs/new
   def new
     @fishing_log = FishingLog.new
+    @lakes = Lake.all
   end
 
   # GET /fishing_logs/1/edit
@@ -22,7 +23,7 @@ class FishingLogsController < ApplicationController
 
   # POST /fishing_logs or /fishing_logs.json
   def create
-    @fishing_log = FishingLog.new(fishing_log_params)
+    @fishing_log = current_user.fishing_logs.new(fishing_log_params)
 
     respond_to do |format|
       if @fishing_log.save
--- app/models/fishing_log.rb
+++ app/models/fishing_log.rb
@@ -1,4 +1,9 @@
 class FishingLog < ApplicationRecord
   belongs_to :user
   belongs_to :lake
+
+  validates :date, presence: true
+  validates :species, presence: true
+  validates :notes, presence: true
+  validates :lures, presence: true
 end
